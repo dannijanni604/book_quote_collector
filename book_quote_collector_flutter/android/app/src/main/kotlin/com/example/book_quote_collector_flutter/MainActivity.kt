413fce684155b00dfeaae9d86b260814122e040f
adnan-wb <aaservices604@gmail.com>
2024-09-11 11:40:16 +0500

    initial-commit

---
+++ book_quote_collector_flutter/android/app/src/main/kotlin/com/example/book_quote_collector_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.book_quote_collector_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ book_quote_collector_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:book_quote_collector_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
